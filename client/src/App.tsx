70d16a04642ec80e3fbf227c753ce4e4b2ddc7a2
Jason Ngo <jsncourn@gmail.com>
2022-04-04 17:40:45 -0700

    Cleared app and menu.tsx, created topbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
+import Menu from './components/Menu/Menu';
 import Home from './pages/home/Home';
 
 function App() {
 
-  const [menuOpen, setMenuOpen] = useState(false); // Determines if menu opens or not
+  const [menuOpen, setMenuOpen] = useState<boolean>(false); // Determines if menu opens or not
 
   return (
     <div className="app">
--- client/src/components/Menu/Menu.tsx
+++ client/src/components/Menu/Menu.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Menu() {
   return (
-    <div>Menu</div>
+    <div className=''>
+      
+      </div>
   )
 }
