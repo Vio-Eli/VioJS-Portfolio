9c988b8b20a593743aada4913ed109361de5ae23
Eli Fischl <vio@ocf.berkeley.edu>
2022-04-06 16:52:04 -0700

    Fixed Menu z-index

--- client/src/components/Menu/Menu.tsx
+++ client/src/components/Menu/Menu.tsx
@@ -6,14 +6,18 @@ interface MenuProps {
   setMenuOpen(menuOpen: boolean): void
 }
 
-
 export default function Menu(props: MenuProps) {
+
+const onClickHandler = () => {
+  props.setMenuOpen(false);
+}
+
   return (
     <div className={"menuWrapper " + (props.menuOpen && "active")}>
       <ul>
-        <li><a href="#">HOME</a></li>
-        <li><a href="#">ABOUT</a></li>
-        <li><a href="#">PROJECTS</a></li>
+        <li><a href="#" onClick={onClickHandler}>HOME</a></li>
+        <li><a href="#" onClick={onClickHandler}>ABOUT</a></li>
+        <li><a href="#" onClick={onClickHandler}>PROJECTS</a></li>
       </ul>
     </div>
   )
