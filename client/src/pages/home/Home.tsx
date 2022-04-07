d5c119da01b12766ac41522205bbb42b4d7711fa
Eli Fischl <vio@ocf.berkeley.edu>
2022-04-06 23:40:29 -0700

    Put the Intro z-index back to 4
    Minor changes to Speed for the scrolling animation

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -16,8 +16,8 @@ export default function Home() {
       {
         snapDestinationY: '100%',
         timeout: 50,
-        duration: 250,
-        threshold: 0.05,
+        duration: 300,
+        threshold: 0.1,
         easing: (t: number) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t),
       },
     )
