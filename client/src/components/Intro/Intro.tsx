7f3a2701b5d2cd52bcf10f81a6322a9baf249361
Jason Ngo <jsncourn@gmail.com>
2022-04-07 14:28:38 -0700

    Added hexagons for the intro so we can add information later
    Co-authored-by: Eli Fischl <Vio-Eli@users.noreply.github.com>

--- client/src/components/Intro/Intro.tsx
+++ client/src/components/Intro/Intro.tsx
@@ -4,11 +4,26 @@ import './Intro.scss';
 export default function Intro() {
   return (
     <div className="introWrapper">
-      <div className="usIntroBox">
-        <span>
-          who we are blah blah blah
-          </span>
+      <div className="left">
+        <div className='top-left'>
+          <div className="hexagon"/>
+          <img className="icon" alt="" src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-image-512.png"/>
+        </div>
+
+        <div className='middle-right'>
+          <div className='hexagon' />
+        </div>
+
+        <div className='bottom-left'>
+          <div className='hexagon' />
+        </div>
+
       </div>
+
+      <div className='right'>
+
+      </div>
+
     </div>
   )
 }
